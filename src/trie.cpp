9d190e0cb05879e5efdf2e9ef0c7ad51fb6ebfd0
r4nx <mod34@yandex.ru>
2020-10-28 02:45:01 +0200

    Another wrong string construction implementation ..
    .. using the queue

--- src/trie.cpp
+++ src/trie.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <queue>
 #include <string>
+#include <utility>
 #include <vector>
 
 void TrieNode::insert(const std::wstring &key)
@@ -20,6 +21,10 @@ std::vector<std::wstring> TrieNode::extract_words(const std::wstring &prefix)
 {
     std::vector<std::wstring> words{prefix};
     std::queue<TrieNode *>    traversal;
+    std::queue<std::pair<wchar_t, bool>>
+         char_queue; // bool to check whether char is the of the word
+    bool new_word_required = false;
+
     traversal.push(this);
 
     while (!traversal.empty())
@@ -27,13 +32,22 @@ std::vector<std::wstring> TrieNode::extract_words(const std::wstring &prefix)
         TrieNode *current_node = traversal.front();
         traversal.pop();
 
-        for (auto &[key, val] : current_node->children)
+        if (!char_queue.empty())
         {
-            words.back().push_back(key);
+            auto [c, is_word_end] = char_queue.front();
+            char_queue.pop();
 
-            if (val.is_word_end)
+            if (new_word_required)
                 words.push_back(prefix);
 
+            words.back().push_back(c);
+
+            new_word_required = is_word_end;
+        }
+
+        for (auto &[key, val] : current_node->children)
+        {
+            char_queue.push({key, val.is_word_end});
             traversal.push(&val);
         }
     }
